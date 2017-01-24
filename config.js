9f15546d2350eed90af3965d76c4c99f6c025e8d
Andrew castro <listerine1989@gmail.com>
2017-01-24 11:52:45 -0500

    latest

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	dburl : "mongodb://panico:Majo0126*@localhost:27017/panico",
+	secret : "$shoply.2016",
+	appPort : 8080,
+	smtp_email : "listerine1989@gmail.com",
+	smtp_password : 'house1989*',
+	smtp_server : 'smtp.gmail.com' 
+}
