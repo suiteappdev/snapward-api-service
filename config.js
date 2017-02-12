f0227d3b6974341c9ac996e19eaaca7a7da2e404
Andrew castro <listerine1989@gmail.com>
2017-02-12 10:45:21 -0500

    latest

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	dburl : "mongodb://panico:panico*@localhost:27017/panico",
+	dburl : "mongodb://reportsUser:12345678@localhost:27017/panico",
 	secret : "$shoply.2016",
 	appPort : 8080,
 	smtp_email : "listerine1989@gmail.com",
